36d04fab666a8718202facfc2d6a02a82cdbe0a0
梁芙陈 <2506730914@qq.com>
2024-06-24 02:41:14 +0800

    栏目模块基本完成

--- src/assets/api/login.js
+++ src/assets/api/login.js
@@ -1,14 +1,24 @@
 import { post } from './index.js';
-
+// 登陆
 export const login = (data) => {
   // 使用绝对路径确保请求的准确性
   return post('/php/index.php', data);
 };
+// 添加分类
 export const addcolum = (data) => {
-  // 使用绝对路径确保请求的准确性
   return post('php/addcolum.php', data);
 };
+// 查询分类列表
 export const searchColum = (data) => {
-  // 使用绝对路径确保请求的准确性
   return post('php/colum.php', data);
+};
+// 修改分类
+export const upColum = (data) => {
+  return post('php/updatecolum.php', data);
+};
+
+// 删除分类
+
+export const delColum = (data) => {
+  return post('php/delcolum.php', data);
 };
